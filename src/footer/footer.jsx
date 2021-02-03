48b0908639987a9e14788201769e2b714101a9f7
hj <jjjjhjjjj123@gmail.com>
2021-02-04 02:36:09 +0900

    Add Footer Component

---
+++ src/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p className={styles.content}>Code your dream</p>
+  </footer>
+);
+
+export default Footer;
