9c0b16e866503e7c4e68929dc626fb2ccedcb4be
hj <jjjjhjjjj123@gmail.com>
2021-02-19 01:07:26 +0900

    Add Editor and Preview

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import EditorAddForm from "../editor_add_form/editor_add_form";
+import EditorInput from "../editor_input/editor_input";
 import styles from "./editor.module.css";
 
-const Editor = (props) => (
-  <div className={styles.editor}>
-    <p>Editor!!!!</p>
-  </div>
+const Editor = ({ cards, addCard }) => (
+  <section className={styles.editor}>
+    <h1 className={styles.title}>Card Maker</h1>
+    {cards.map((card) => (
+      <EditorInput key={card.id} card={card} />
+    ))}
+    <EditorAddForm onAdd={addCard} />
+  </section>
 );
 
 export default Editor;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import Header from "../header/header";
 import Editor from "../editor/editor";
@@ -8,6 +8,41 @@ import styles from "./maker.module.css";
 
 const Maker = ({ auth }) => {
   const history = useHistory();
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Ellie",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "Ellie2",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: "ellie.png",
+    },
+    {
+      id: "3",
+      name: "Ellie3",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+  ]);
 
   const onLogout = () => {
     auth //
@@ -22,12 +57,17 @@ const Maker = ({ auth }) => {
       user || history.push("/");
     });
   }, []);
+
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
   return (
     <div className={styles.maker}>
       <Header onLogout={onLogout} />
       <section className={styles.makerMain}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} addCard={addCard} />
+        <Preview cards={cards} />
       </section>
       <Footer />
     </div>
